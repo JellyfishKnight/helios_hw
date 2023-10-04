eb28dd306bae197bcbb61fc136557e2e9043b25c
JellyfishKnight <1002984726@qq.com>
2023-10-04 22:09:07 +0800

    motor hw version 1.0

--- motor_hw/hardware/include/MotorHardware.hpp
+++ motor_hw/hardware/include/MotorHardware.hpp
@@ -1,4 +1,14 @@
 // created by liuhan on 2023/9/10
+// Submodule of HeliosRobotSystem
+// for more see document: https://swjtuhelios.feishu.cn/docx/MfCsdfRxkoYk3oxWaazcfUpTnih?from=from_copylink
+/*
+ * ██   ██ ███████ ██      ██  ██████  ███████
+ * ██   ██ ██      ██      ██ ██    ██ ██
+ * ███████ █████   ██      ██ ██    ██ ███████
+ * ██   ██ ██      ██      ██ ██    ██      ██
+ * ██   ██ ███████ ███████ ██  ██████  ███████
+ *
+ */
 #pragma once
 
 #include <cstdint>
